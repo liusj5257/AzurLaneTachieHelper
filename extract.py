e36e8eecee494d4ec08baf65d2d1410b46a57653
liusj5257 <lius5257@gmail.com>
2023-07-06 14:56:38 +0800

    def extract

--- extract.py
+++ extract.py
@@ -5,24 +5,26 @@ from PIL import Image
 from UnityPy import Environment
 from UnityPy.classes import Sprite
 
-from src.utility import check_dir
+from .src.utility import check_dir
 
-outdir = "loadingbg_img"
-check_dir(outdir)
-for file in os.listdir("loadingbg"):
-    env: Environment = UnityPy.load(os.path.join("loadingbg", file))
-    for k, v in env.container.items():
-        sprite: Sprite = v.read()
-        name = sprite.name
-        img: Image.Image = sprite.image
-        if k.startswith("assets/artresource/atlas/loadingbg/"):
-            target = os.path.join(outdir, f"{name}_h.png")
-        elif k.startswith("assets/rescategories/fanhx/artresource/atlas/loadingbg/"):
-            target = os.path.join(outdir, f"{name}.png")
-        elif k.startswith("assets/rescategories/jp/artresource/atlas/loadingbg/"):
-            target = os.path.join(outdir, f"{name}_jp.png")
-        else:
-            print (k)
-            # raise ValueError(k)
-        print("[INFO] Dumping:", target)
-        img.save(target)
+
+def extract(src='loadingbg', outdir='loadingbg_img'):
+    check_dir(outdir)
+    for file in os.listdir(src):
+        # print (file)
+        env: Environment = UnityPy.load(os.path.join(src, file))
+        for k, v in env.container.items():
+            sprite: Sprite = v.read()
+            name = sprite.name
+            img: Image.Image = sprite.image
+            if k.startswith("assets/artresource/atlas/loadingbg/"):
+                target = os.path.join(outdir, f"{name}_h.png")
+            elif k.startswith("assets/rescategories/fanhx/artresource/atlas/loadingbg/"):
+                target = os.path.join(outdir, f"{name}.png")
+            elif k.startswith("assets/rescategories/jp/artresource/atlas/loadingbg/"):
+                target = os.path.join(outdir, f"{name}_jp.png")
+            else:
+                print (k)
+                # raise ValueError(k)
+            print("[INFO] Dumping:", target)
+            img.save(target)
